148a8ec48719bbfa42b1e6c18fd2716b2a200ccf
Alexander Temper <tmpr@pop-os.localdomain>
2020-12-01 08:17:45 +0100

    Fixed Bug

---
+++ src/2020/2/solutions.py
@@ -0,0 +1,9 @@
+def part_1(input_data: str):
+    """Return first solution of puzzle."""
+    if input_data == "": return ""
+    raise NotImplementedError
+
+def part_2(input_data: str):
+    """Return second solution of puzzle."""
+    if input_data == "": return ""
+    raise NotImplementedError
