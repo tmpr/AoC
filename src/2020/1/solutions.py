9ef90c61e640bfb2f8a1b6cb6748892c12c0aceb
Alexander Temper <tmpr@pop-os.localdomain>
2020-12-01 08:20:28 +0100

    changed variable names

--- src/2020/1/solutions.py
+++ src/2020/1/solutions.py
@@ -2,12 +2,12 @@ from itertools import combinations
 from functools import reduce
 from operator import mul
 
-def part_1(input_data: str, nums=2):
+def part_1(input_data: str, n_summands=2):
     """Return first solution of puzzle."""
     if input_data == "": return ""
     
     numbers = [int(line) for line in input_data.splitlines()]
-    for comb in combinations(numbers, nums):
+    for comb in combinations(numbers, n_summands):
         if sum(comb) == 2020:
             return reduce(mul, comb)
 
@@ -16,4 +16,4 @@ def part_2(input_data: str):
     """Return second solution of puzzle."""
     if input_data == "": return ""
 
-    return part_1(input_data, nums=3)
+    return part_1(input_data, n_summands=3)
