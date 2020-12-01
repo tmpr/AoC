0a3750439cca47c0aa308ed123b9ae9c33450409
Alexander Temper <tmpr@pop-os.localdomain>
2020-12-01 10:02:06 +0100

    Changed template

--- src/2020/1/solutions.py
+++ src/2020/1/solutions.py
@@ -3,9 +3,7 @@ from functools import reduce
 from operator import mul
 
 def part_1(input_data: str, n_summands=2):
-    """Return first solution of puzzle."""
-    if input_data == "": return ""
-    
+    """Return first solution of puzzle."""    
     numbers = [int(line) for line in input_data.splitlines()]
     for comb in combinations(numbers, n_summands):
         if sum(comb) == 2020:
@@ -14,6 +12,4 @@ def part_1(input_data: str, n_summands=2):
 
 def part_2(input_data: str):
     """Return second solution of puzzle."""
-    if input_data == "": return ""
-
     return part_1(input_data, n_summands=3)
