083524a08b79f13e65d445335b51ec1f29985520
Alexander Temper <tmpr@pop-os.localdomain>
2020-12-01 08:11:02 +0100

    Day 1 done

--- 2020/1/solutions.py
+++ 2020/1/solutions.py
@@ -1,11 +1,19 @@
-def part_1(input_data: str):
+from itertools import combinations
+from functools import reduce
+from operator import mul
+
+def part_1(input_data: str, nums=2):
     """Return first solution of puzzle."""
-    # Make empty testcases run for convencience.
     if input_data == "": return ""
-    raise NotImplementedError
+    
+    numbers = [int(line) for line in input_data.splitlines()]
+    for comb in combinations(numbers, nums):
+        if sum(comb) == 2020:
+            return reduce(mul, comb)
+
 
 def part_2(input_data: str):
     """Return second solution of puzzle."""
-    # Make empty testcases run for convencience.
     if input_data == "": return ""
-    raise NotImplementedError
+
+    return part_1(input_data, nums=3)
