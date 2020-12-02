5bda6778a8e4cb17a05872bc393364eb8cb057fd
Alexander Temper <tmpr@pop-os.localdomain>
2020-12-02 07:15:15 +0100

    Reformat

--- src/2020/2/solutions.py
+++ src/2020/2/solutions.py
@@ -1,32 +1,32 @@
 import re
 
-RULE_PATTERN = re.compile(r'(\d*)-(\d*) (.)')
+from operator import xor
+
+RULE_PATTERN = re.compile(r'(\d*)-(\d*) (.): (.*)')
+
 
 def password_policies(input_data, part_a=True):
-    found = 0
+    found = 0   
     for line in input_data.splitlines():
-        
+
         rule = re.match(RULE_PATTERN, line)
         low = int(rule.group(1))
         high = int(rule.group(2))
         letter = rule.group(3)
-        
-        rule = re.compile('\d*-\d* ' + letter + ': (.*)')
-        valid = re.search(rule, line)
-        letters = valid.group(1)
+        letters = rule.group(4)
 
         if part_a:
-            found += bool(low <= letters.count(letter) <= high)
+            found += low <= letters.count(letter) <= high
         else:
-            found += bool(letters[low - 1] == letter 
-                          and letters[high - 1] != letter)
+            found += xor(letters[low - 1] == letter,
+                         letters[high - 1] == letter)
 
     return found
+
 
 def part_1(input_data: str):
     """Return first solution of puzzle."""
     return password_policies(input_data)
-
 
 
 def part_2(input_data: str):
