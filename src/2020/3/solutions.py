72b2e5066fa66d1541e6fb384a06ee3f4f65b360
Alexander Temper <tmpr@pop-os.localdomain>
2020-12-03 06:42:11 +0100

    Day 3

--- src/2020/3/solutions.py
+++ src/2020/3/solutions.py
@@ -1,7 +1,34 @@
-def part_1(input_data: str):
+import re
+
+from operator import mul
+from functools import reduce
+
+import numpy as np
+
+
+def string_to_matrix(input_data):
+    return np.array([
+        np.array([char for char in line])
+        for line in input_data.splitlines()])
+
+
+def part_1(input_data: str, dv=3, dh=1):
     """Return first solution of puzzle."""
-    raise NotImplementedError
+    
+    field = string_to_matrix(input_data)
+    height, width = field.shape
+    n_trees = 0
+    v = 0
+    
+    for h in range(0, height, dh):
+        n_trees += field[h][v] == '#'
+        v += dv
+        v %= width
+
+    return n_trees
+
 
 def part_2(input_data: str):
     """Return second solution of puzzle."""
-    raise NotImplementedError
+    configs = (1, 1), (3, 1), (5, 1), (7, 1), (1, 2)
+    return reduce(mul, (part_1(input_data, *config) for config in configs))
