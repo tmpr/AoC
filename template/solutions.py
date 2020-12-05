4236a858445c5d0ba3a003236ce3bf22feb9b107
Alexander Temper <tmpr@pop-os.localdomain>
2020-12-05 06:40:09 +0100

    untrack testexamples

--- template/solutions.py
+++
@@ -1,7 +0,0 @@
-def part_1(input_data: str):
-    """Return first solution of puzzle."""
-    raise NotImplementedError
-
-def part_2(input_data: str):
-    """Return second solution of puzzle."""
-    raise NotImplementedError
