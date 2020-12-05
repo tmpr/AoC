d65aaa6fe8ec1fb3bba3eef8ecd5e2c8395d1dc5
Alexander Temper <tmpr@pop-os.localdomain>
2020-12-05 06:40:29 +0100

    track solution

---
+++ src/2020/3/solutions.py
@@ -0,0 +1,31 @@
+import re
+
+import numpy as np
+
+
+def string_to_matrix(input_data):
+    return np.array([
+        np.array([char for char in line])
+        for line in input_data.splitlines()])
+
+
+def part_1(input_data: str, dv=3, dh=1):
+    """Return first solution of puzzle."""
+    
+    field = string_to_matrix(input_data)
+    height, width = field.shape
+    n_trees = 0
+    v = 0
+    
+    for h in range(0, height, dh):
+        n_trees += field[h][v] == '#'
+        v += dv
+        v %= width
+
+    return n_trees
+
+
+def part_2(input_data: str):
+    """Return second solution of puzzle."""
+    configs = (1, 1), (3, 1), (5, 1), (7, 1), (1, 2)
+    return np.prod(part_1(input_data, *config) for config in configs)
---
+++ src/2020/5/solutions.py
@@ -0,0 +1,32 @@
+def bisect(array: list, u=True) -> list:
+    mid = len(array) // 2
+    return array[:mid] if not u else array[mid:]
+
+
+def seat_id(row: int, col: int) -> int:
+    return (row * 8) + col
+
+
+def calc_seat_id(code: str) -> int:
+    rows = list(range(128))
+    cols = list(range(8))
+    for c in code[:7]:
+        rows = bisect(rows, c == 'B')
+    for c in code[7:]:
+        cols = bisect(cols, c == 'R')
+    return seat_id(rows[0], cols[0])
+
+
+def part_1(input_data: str):
+    """Return first solution of puzzle."""
+    return max(calc_seat_id(line) for line in input_data.splitlines())
+
+
+def part_2(input_data: str):
+    """Return second solution of puzzle."""
+    ids = list(calc_seat_id(line) for line in input_data.splitlines())
+    max_ = max(ids)
+
+    print(set(range(-1, max_)) - set(ids))
+    # Pretty manual solution but idk how to compute that algorithmically :D
+    return int(input('Enter number at that sticks out: '))
